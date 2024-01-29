e3d28a4364237fb10c7da6851370388bcca392d7
Imad Selka <imadselkawi133@gmail.com>
2024-01-29 20:36:35 +0100

    feat:  add 3 buttons on bottom navigation bar besides that there is a text and  a button on center of the body

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,52 @@ class MyApp extends StatelessWidget {
           false, // to remove debug banner so annoying !!!
       home: Scaffold(
         appBar: AppBar(
-          title: Text('slm'),
-          backgroundColor: Colors.blue,
+          title: const Text('slm'),
+          backgroundColor: const Color.fromARGB(255, 29, 79, 119),
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello, World!'),
+              SizedBox(
+                  height:
+                      20), // Optional: Add some space between text and button
+              ElevatedButton(
+                onPressed: null,
+                child: Text('Your Button Text'),
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: 'Like',
+              icon: Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 24.0,
+                semanticLabel: 'Text to announce in accessibility modes',
+              ),
+            ),
+            BottomNavigationBarItem(
+              label: 'Audio',
+              icon: Icon(
+                Icons.audiotrack,
+                color: Color.fromARGB(255, 33, 158, 37),
+                size: 30.0,
+                semanticLabel: 'Audio track',
+              ),
+            ),
+            BottomNavigationBarItem(
+                label: 'Settings',
+                icon: Icon(
+                  Icons.settings,
+                  size: 30.0,
+                  semanticLabel: 'Settings',
+                ))
+          ],
         ),
       ),
     );
