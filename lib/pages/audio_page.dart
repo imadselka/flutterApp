80552dd85dca82baa9d505c21ad668c988c9b270
Imad Selka <imadselkawi133@gmail.com>
2024-02-01 19:56:23 +0100

    fix the classes

--- lib/pages/audio_page.dart
+++ lib/pages/audio_page.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class LikePage extends StatelessWidget {
-  const LikePage({super.key});
+class AudioPage extends StatelessWidget {
+  const AudioPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: const Text('Like Page Content'),
+      child: const Text('Audio Page Content'),
     );
   }
 }
